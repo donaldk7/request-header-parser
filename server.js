f121f692af90bf3ab115788c6f28fd7ab94a3b8f
Donny <donaldk@shaw.ca>
2016-11-29 21:26:39 +0000

    working app

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+    var str = JSON.stringify(req.headers)   // req.headers returns object type, String() will not work
+    var json = JSON.parse(str);
+    var browserInfo = '';
+    browserInfo = '{"IP Address": "' + json["x-forwarded-for"] + '", ' + 
+                    '"Language": "' + json["accept-language"].substring(0,5) + '", ' +
+                    '"Software": "' + json["user-agent"] + '"}';
+    
+    
+    res.end(browserInfo);
+
+});
+
+app.listen(8080, function(req, res) {
+  console.log('Request Header Parser')
+});
+
