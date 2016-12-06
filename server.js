eec6d9b5d380a84eb431fc8e41f3850f5fb82c1f
Donny <donaldk@shaw.ca>
2016-12-06 00:28:17 +0000

    test

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    var str = JSON.stringify(req.headers)  
-    var json = JSON.parse(str)
-    var browserInfo = ''
-    browserInfo = '{"IP Address": "' + json["x-forwarded-for"] + '", ' + 
-                    '"Language": "' + json["accept-language"].substring(0,5) + '", ' +
-                    '"Software": "' + json["user-agent"] + '"}'
+  var ipaddress = req.get('x-forwarded-for')
+  var lang = req.get('accept-language')
+  var software = req.get('user-agent')
+   // var str = JSON.stringify(req.headers)  
+    //var json = JSON.parse(str)
+    //var browserInfo = ''
+    //browserInfo = '{"IP Address": "' + json["x-forwarded-for"] + '", ' + 
+     //               '"Language": "' + json["accept-language"].substring(0,5) + '", ' +
+     //               '"Software": "' + json["user-agent"] + '"}'
     
     
-    res.send(browserInfo)
+    //res.send(browserInfo)
+    res.send(ipaddress + lang + software)
 
 })
 
