6e9464486ccd972b158a7c4a7078cea2d5358265
Donny <donaldk@shaw.ca>
2016-12-06 00:17:06 +0000

    revision 2

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    var str = JSON.stringify(req.headers)   // req.headers returns object type, String() will not work
-    var json = JSON.parse(str);
-    var browserInfo = '';
+    var str = JSON.stringify(req.headers)  
+    var json = JSON.parse(str)
+    var browserInfo = ''
     browserInfo = '{"IP Address": "' + json["x-forwarded-for"] + '", ' + 
                     '"Language": "' + json["accept-language"].substring(0,5) + '", ' +
-                    '"Software": "' + json["user-agent"] + '"}';
+                    '"Software": "' + json["user-agent"] + '"}'
     
     
-    res.send(browserInfo);
+    res.send(browserInfo)
 
-});
+})
 
 app.listen(process.env.PORT || 8080, function(req, res) {
   console.log('Request Header Parser')
-});
+})
 
